90bda5482842248f171a5f8e4d75a839f4efde38
Damian Salata <damiansalata10@gmail.com>
2019-03-08 19:51:07 +0100

    Fix spelling

--- SignalProcessingView/MainWindow.xaml.cs
+++ SignalProcessingView/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SingalProcessingView
+namespace SignalProcessingView
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
