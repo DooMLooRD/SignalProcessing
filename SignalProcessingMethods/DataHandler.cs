1948604d9a15c898f97699348404b44a2bca2819
Damian Salata <damiansalata10@gmail.com>
2019-03-09 22:09:50 +0100

    Add Histogram

--- SignalProcessingMethods/DataHandler.cs
+++ SignalProcessingMethods/DataHandler.cs
@@ -11,6 +11,31 @@ namespace SignalProcessingMethods
         public List<double> PointsX { get; set; }
         public List<double> PointsY { get; set; }
 
+        public double GetMinX()
+        {
+            return PointsX.Min();
+        }
+        public double GetMaxX()
+        {
+            return PointsX.Max();
+        }
+        public List<(double, double, int)> GetDataForHistogram(int count)
+        {
+            List<(double, double, int)> result = new List<(double, double, int)>(count);
+            double max = PointsY.Max();
+            double min = PointsY.Min();
+
+            double range = max - min;
+            double interval = range / count;
+            for (int i = 0; i < count; i++)
+            {
+                int points = PointsY.Count(n => n >= min + interval * i && n < min + interval * (i + 1));
+                result.Add((Math.Round(min + interval * i, 2), Math.Round(min + interval * (i + 1), 2), points));
+            }
+
+            return result;
+        }
+
         public void LoadFromFile(string filePath)
         {
 
