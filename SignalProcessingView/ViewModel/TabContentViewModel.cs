842ac6ba42f962d85340f6726f11f327694e47bd
Damian Salata <damiansalata10@gmail.com>
2019-03-21 15:40:53 +0100

    Fix tab control reloading

--- SignalProcessingView/ViewModel/TabContentViewModel.cs
+++ SignalProcessingView/ViewModel/TabContentViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using LiveCharts;
 using LiveCharts.Configurations;
 using LiveCharts.Defaults;
@@ -32,6 +35,7 @@ namespace SignalProcessingView.ViewModel
         public double RMSSignal { get; set; }
 
         public ICommand Histogram { get; set; }
+        public ICommand SaveCharts { get; set; }
 
         public int SliderValue
         {
@@ -49,8 +53,136 @@ namespace SignalProcessingView.ViewModel
         {
             Data = new DataHandler();
             Histogram = new RelayCommand<int>(LoadHistogram);
-
+            SaveCharts = new RelayCommand(SaveChartsToFile);
         }
+
+        #region Save Charts
+
+        public void SaveChartsToFile()
+        {
+            var chart = new LiveCharts.Wpf.CartesianChart()
+            {
+                Background = new SolidColorBrush(Colors.White),
+                DisableAnimations = true,
+                Width = 1920,
+                Height = 1080,
+                DataTooltip = null,
+                Hoverable = false,
+
+            };
+            var histogram = new LiveCharts.Wpf.CartesianChart()
+            {
+                Background = new SolidColorBrush(Colors.White),
+                DisableAnimations = true,
+                Width = 1920,
+                Height = 1080,
+                DataTooltip = null,
+                Hoverable = false,
+            };
+            var mapper = Mappers.Xy<PointXY>()
+                   .X(value => value.X)
+                   .Y(value => value.Y);
+            ChartValues<PointXY> values = new ChartValues<PointXY>();
+            List<double> pointsX;
+            List<double> pointsY;
+            if (Data.FromSamples)
+            {
+                pointsX = Data.SamplesX;
+                pointsY = Data.Samples;
+            }
+            else
+            {
+                pointsX = Data.PointsX;
+                pointsY = Data.PointsY;
+            }
+            for (int i = 0; i < pointsX.Count; i++)
+            {
+                values.Add(new PointXY(pointsX[i], pointsY[i]));
+            }
+
+            if (IsScattered || Data.FromSamples)
+            {
+                chart.Series = new SeriesCollection(mapper)
+                    {
+                        new ScatterSeries()
+                        {
+                            PointGeometry = new EllipseGeometry(),
+                            StrokeThickness = 10,
+                            Values = values,
+                        }
+                    };
+            }
+            else
+            {
+                chart.Series = new SeriesCollection(mapper)
+                    {
+                        new LineSeries()
+                        {
+                            LineSmoothness = 0,
+                            StrokeThickness = 3,
+                            Fill = Brushes.Transparent,
+                            PointGeometry = null,
+                            Values = values,
+                        }
+                    };
+            }
+
+
+            var histogramResults = Data.GetDataForHistogram(SliderValue);
+
+            //HistogramStep = (int)Math.Ceiling(SliderValue / 20.0);
+            histogram.Series = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Values = new ChartValues<int> (histogramResults.Select(n=>n.Item3)),
+                    ColumnPadding = 0,
+
+                }
+            };
+            //Labels = histogramResults.Select(n => n.Item1 + " to " + n.Item2).ToArray();
+            chart.AxisX = new AxesCollection() { new Axis() { FontSize = 30, } };
+            chart.AxisY = new AxesCollection() { new Axis() { FontSize = 30, } };
+
+            histogram.AxisY = new AxesCollection() { new Axis() { FontSize = 30, } };
+            histogram.AxisX = new AxesCollection() { new Axis() { FontSize = 30, Labels = histogramResults.Select(n => n.Item1 + " to " + n.Item2).ToArray(), LabelsRotation = 60, Separator = new LiveCharts.Wpf.Separator() { Step = (int)Math.Ceiling(SliderValue / 20.0) } } };
+
+            var viewbox = new Viewbox();
+            viewbox.Child = chart;
+            viewbox.Measure(chart.RenderSize);
+            viewbox.Arrange(new Rect(new Point(0, 0), chart.RenderSize));
+            chart.Update(true, true); //force chart redraw
+            viewbox.UpdateLayout();
+
+            var histViewbox = new Viewbox();
+            histViewbox.Child = histogram;
+            histViewbox.Measure(histogram.RenderSize);
+            histViewbox.Arrange(new Rect(new Point(0, 0), histogram.RenderSize));
+            histogram.Update(true, true); //force chart redraw
+            histViewbox.UpdateLayout();
+
+            SaveToPng(chart, "chart.png");
+            SaveToPng(histogram, "histogram.png");
+            MessageBox.Show("Files saved", "Done", MessageBoxButton.OK, MessageBoxImage.Information);
+            //png file was created at the root directory.
+        }
+
+        private void SaveToPng(FrameworkElement visual, string fileName)
+        {
+            var encoder = new PngBitmapEncoder();
+            EncodeVisual(visual, fileName, encoder);
+        }
+
+        private static void EncodeVisual(FrameworkElement visual, string fileName, BitmapEncoder encoder)
+        {
+            var bitmap = new RenderTargetBitmap((int)visual.ActualWidth, (int)visual.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+            bitmap.Render(visual);
+            var frame = BitmapFrame.Create(bitmap);
+            encoder.Frames.Add(frame);
+            using (var stream = File.Create(fileName)) encoder.Save(stream);
+        }
+
+        #endregion
 
         public void DrawCharts()
         {
@@ -85,7 +217,8 @@ namespace SignalProcessingView.ViewModel
                         {
                             PointGeometry = new EllipseGeometry(),
                             StrokeThickness = 5,
-                            Values = values
+                            Values = values,
+
                         }
                     };
                 }
@@ -99,7 +232,8 @@ namespace SignalProcessingView.ViewModel
                             StrokeThickness = 0.5,
                             Fill = Brushes.Transparent,
                             PointGeometry = null,
-                            Values = values
+                            Values = values,
+
                         }
                     };
                 }
@@ -112,7 +246,8 @@ namespace SignalProcessingView.ViewModel
                     new ColumnSeries
                     {
                         Values = new ChartValues<int> (histogramResults.Select(n=>n.Item3)),
-                        ColumnPadding = 0
+                        ColumnPadding = 0,
+                        CacheMode = new BitmapCache()
 
                     }
                 };
@@ -143,10 +278,11 @@ namespace SignalProcessingView.ViewModel
                     {
                         Values = new ChartValues<int> (histogramResults.Select(n=>n.Item3)),
                         ColumnPadding = 0,
-
+                        CacheMode = new BitmapCache()
                     }
                 };
                 Labels = histogramResults.Select(n => n.Item1 + " to " + n.Item2).ToArray();
+
             }
 
         }
