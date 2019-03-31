98645358492f24dc9e88bd6321dec77aabd73214
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-03-31 17:25:46 +0200

    Add quantization

---
+++ SignalProcessingZad2/Quantization.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalProcessingZad2
+{
+    public class Quantization
+    {
+        public List<double> Quantize(List<double> values, int levels)
+        {
+            return values.Select(n => Math.Round(n * levels) / levels).ToList();
+        }
+    }
+}
