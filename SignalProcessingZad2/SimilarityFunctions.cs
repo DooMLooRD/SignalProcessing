07afc64a73da3f8d32f1720258dce7198fbd6a4a
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-03-31 21:27:28 +0200

    Add zero order hold and sinc

--- SignalProcessingZad2/SimilarityFunctions.cs
+++ SignalProcessingZad2/SimilarityFunctions.cs
@@ -29,5 +29,15 @@ namespace SignalProcessingMethods
         {
             return original.Zip(reconstructed, (d, d1) => Math.Abs(d - d1)).Max(); 
         }
+
+        public double CalculateSNRAC(int levels)
+        {
+            return 6.02 * (Math.Log(levels, 2)) + 1.76;
+        }
+
+        public double CalculateENOB(List<double> original, List<double> reconstructed)
+        {
+            return (CalculateSNR(original, reconstructed) - 1.76) / 6.02;
+        }
     }
 }
