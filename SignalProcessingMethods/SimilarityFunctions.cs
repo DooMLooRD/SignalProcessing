abae2f4e67a795d1b75b884c83107bb388f5facf
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-03-31 16:49:38 +0200

    Add similarity functions

---
+++ SignalProcessingMethods/SimilarityFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalProcessingMethods
+{
+    public class SimilarityFunctions
+    {
+        public double CalculateMSE(List<double> original, List<double> reconstructed)
+        {
+            return original.Zip(reconstructed, (d, d1) => Math.Pow(d - d1, 2)).Sum()/ reconstructed.Count;
+        }
+
+        public double CalculateSNR(List<double> original, List<double> reconstructed)
+        {
+            double squareSum = original.Sum(n => Math.Pow(n, 2));
+            double differenceSum = original.Zip(reconstructed, (d, d1) => Math.Pow(d - d1, 2)).Sum();
+            return 10 * Math.Log10(squareSum/differenceSum);
+        }
+
+        public double CalculatePNSR(List<double> original, List<double> reconstructed)
+        {
+            return 10 * Math.Log10(original.Max()/CalculateMSE(original, reconstructed));
+        }
+
+        public double CalculateMD(List<double> original, List<double> reconstructed)
+        {
+            return original.Zip(reconstructed, (d, d1) => Math.Abs(d - d1)).Max(); 
+        }
+    }
+}
