a4ea9b7c3d16164e7d820b1b8a28de9a7730b04c
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2019-05-06 12:33:04 +0200

    Add correlation

---
+++ SignalProcessingZad3/Correlation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalProcessingZad3
+{
+    public class Correlation
+    {
+        public static List<double> ComputeSignal(List<double> signal1, List<double> signal2)
+        {
+            List<double> signal = signal1.ToList();
+            signal.Reverse();
+            return Convolution.ComputeSignal(signal, signal2);
+        }
+    }
+}
